ee8929d04734ac2f9c0dd7e0181cb911a3a29305
Kewde <code@shadowproject.io>
2017-09-14 11:44:33 +0000

    Add informative comments

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+/*
+  Informative comments provided by https://www.blackhat.com/docs/us-17/thursday/us-17-Carettoni-Electronegativity-A-Study-Of-Electron-Security-wp.pdf
+*/
+
 const {app, BrowserWindow} = require('electron');
 const path = require('path');
 
@@ -5,9 +9,31 @@ let win
 app.on('ready', () => {
   win = new BrowserWindow({
     webPreferences: {
+      /*
+        nodeIntegration is disabled, JavaScriptis unable to leverage Node.js primitives and modules.
+      */
       nodeIntegration: false,
+      
+      /*
+         A sandboxed renderer does not have a Node.js environment running (with the  exception  of  preload  scripts)  
+         and  the  renderers  can  only  make  changes  to  the system by delegating tasks to the main process via IPC. 
+      */
       sandbox: true,
+      
+      /*
+      contextIsolation introduces JavaScript context isolation for preload scripts, as implemented  in 
+      Chrome Content Scripts.  This option should be used when loading potentially untrusted resources  
+      to ensure that the  loaded content cannot tamper with the preload script and any Electron  
+      APIs being used. The preload script will still have access to global variables, but it  
+      will use its own set of JavaScript   builtins (Array, Object, JSON, etc.) and will be isolated 
+      from any changes  made to the global environment by the loaded page. 
+      */
       contextIsolation: true,
+      
+      /*
+        The preload script binds a specific function to the window. The function has the ability to
+        execute IPC messages without giving untrusted content complete access to ipcRenderer.
+      */
       preload: path.join(__dirname, 'preload.js')
     }
   })
